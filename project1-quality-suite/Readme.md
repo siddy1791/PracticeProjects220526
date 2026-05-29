97bf545b635801bd8e84899f15ee7b780193b0c8
Siddharth K <snipersiddy@gmail.com>
2026-05-29 21:34:57 +0530

    Project 1 ----> COMPLETE!!!

--- project1-quality-suite/Readme.md
+++ project1-quality-suite/Readme.md
@@ -1,3 +1,9 @@
 In compose file i have removed external:true from below volumes section as \
 I want Podman to automatically create the volume the moment you start the container \
 (and clean it up if you delete the stack)
+
+
+⚖️ Rule of thumb
+dump() → write JSON directly to a file.
+
+dumps() → return JSON as a string.
--- project1-quality-suite/src/validator.py
+++ project1-quality-suite/src/validator.py
@@ -1,6 +1,7 @@
 import yaml
 from pathlib import Path
 from db_connector import *
+import json
 
 class readConfig:
 
@@ -12,33 +13,67 @@ class readConfig:
             data = yaml.safe_load(ymlfile)
             return data
         
-    def rules_check(self,df,rules,columns):
+    def rules_check(self,df,rule_config):
+        rule_type = rule_config['type']
+        print(f"<< RULE TYPE: {rule_type} >>")
+        columns = rule_config['columns']
+        print(f"<< COLUMNS: {columns} >>")
         
-        if rules == "null_check":
-            print("Null Check data::>>>>>>>>>")
-            return df[(df['customer_id'].isnull()) | (df['order_id'].isnull())]
-        elif rules == 'duplicate_check':
-            print("duplicate check inside the dataframe :>>>>>>>>>>>>>>>>>>>>>")
-            return df[df['order_id'].duplicated()]
+        if rule_type == "null_check":
+            print("<<<<<<<<<<<<<<<< Null Check data::>>>>>>>>>")
+            return df[df[columns].isnull().any(axis=1)]
+        
+        elif rule_type == 'duplicate_check':
+            print("<<<<<<<<<<<<<<<<<<<<<<< duplicate check inside the dataframe :>>>>>>>>>>>>>>>>>>>>>")
+            # return df[df[columns].duplicated()]
+            return df[df.duplicated(subset=columns)]
+        
+        elif rule_type == 'value_range_check':
+            print("<<<<<<<<<<<<<<<<<<<<<< Outlier checks inside the DF >>>>>>>>>>>>>>>>>>>>>>>>>")
+            target_column = columns[0] # its best for single column
+
+            max_val = rule_config['max']
+            min_val = rule_config['min']
+
+            return df[(df[target_column] > max_val) | (df[target_column] < min_val)]
         else:
-            print("Range check inside the dataframe :>>>>>>>>>>>>>>>>>>>>>")
-            return df[(df['total_amount'] > 50000) | (df['total_amount'] < 0)]
+            raise ValueError("NO rules matched")
 
 
 if  __name__ == "__main__":
-    # base_dir = os.path.dirname(os.path.abspath(__file__))
+    final_result_op = []
+
     base_dir = Path(__file__).resolve().parent.parent       # >>>>>> ITS WINDOWS PATH
-    config_path = base_dir / "config" / "validation_rules.yaml"
-    fileRead = readConfig(config_path)
+    validationRules_path = base_dir / "config" / "validation_rules.yaml"
+    report_path = base_dir / "docs" / "validation_report.json"
+    fileRead = readConfig(validationRules_path)
     yaml_data = fileRead.read_ymlfile()
     print(yaml_data)
-    print(f"Database: {yaml_data['databases'][0]['name']}, table_name: {yaml_data['databases'][0]['tables'][0]['name']}")
+
+    Database = yaml_data['databases'][0]['name']
+    table_name = yaml_data['databases'][0]['tables'][0]['name']
+
     num_of_rules = len(yaml_data['databases'][0]['tables'][0]['rules'])
     df = db_connection('postgresql','psycopg2','admin','postgres123','localhost','5432','project1')
     for i in range(num_of_rules):
-        print(f"rules: {yaml_data['databases'][0]['tables'][0]['rules'][i]['type']}")
-        print(f"column_list: {yaml_data['databases'][0]['tables'][0]['rules'][i]['columns']}")
-        rules_output = fileRead.rules_check(df,f"{yaml_data['databases'][0]['tables'][0]['rules'][i]['type']}")
-        print(f"OUTPUT RULES {i+1} >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        print(rules_output)
-    print(config_path)
+        final_result_op.append({'DataBase':Database,'TableName':table_name})
+        rules_config = yaml_data['databases'][0]['tables'][0]['rules'][i]
+        final_result_op[i]['rule_type'] = rules_config['type']
+        final_result_op[i]['target_columns'] = rules_config['columns']
+        final_result_op[i]['severity_level'] = rules_config['severity']
+         
+        rules_output = fileRead.rules_check(df,rules_config)
+        print(f"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< OUTPUT RULES {i+1} >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        final_result_op[i]['Num_of_rows_failed'] = len(rules_output)
+        failed_records = rules_output.to_dict(orient='records')
+        if len(rules_output) == 0:
+            final_result_op[i]["Failed_records"] = "NO FAILED RECORDS FOUND"
+            final_result_op[i]['status'] = "WARNING"
+        else:
+            final_result_op[i]["Failed_records"] = failed_records
+            final_result_op[i]['record_status'] = "PRESENT"
+        print(final_result_op)
+
+    with open(report_path,"w") as f:
+        json.dump(final_result_op,f,indent=4,default=str)
+        
